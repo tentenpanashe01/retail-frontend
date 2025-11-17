1e472e2b5e48e20a479c0a87f5b28ccb599a2cf0
tentenpanashe01 <149800126+tentenpanashe01@users.noreply.github.com>
2025-11-17 09:40:39 +0200

    Update baseURL to remove fallback value

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ console.log("api.js loaded");
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
